b1bc5592a844fa9221a4ae8f467cbcb5026d942c
knlss <kauanleosilva2008@gmail.com>
2024-05-08 13:06:50 -0300

    up

--- utils/script.py
+++ utils/script.py
@@ -12,31 +12,33 @@ import openpyxl
 
 class Chrome():
     def __init__(self):
-        self.actions = ActionChains()
-        chrome_options = Options()
+        self.chrome_options = Options()
+        self.archive = openpyxl.load_workbook('utils/src/accounts.xlsx')
+        self.chrome_options.add_argument("--incognito")
 
-        self.spreadsheet = openpyxl.load_workbook('caminho/para/o/arquivo/exemplo.xlsx').active
-        self.rMax = self.spreadsheet["E6"]
+        self.driver = None
+        self.actions = None
+        self.wait = None
 
-        self.textToSearch = self.spreadsheet["E3"]
+    def obtainEmails(self, rMin, rMax):
+        spreadsheet = self.archive["Accounts"]
+        credentials = {}
+        for row in spreadsheet.iter_rows(min_row=rMin, max_row=rMax, min_col=2, max_col=3):
 
-        chrome_options.add_experimental_option("detach", True)
-        self.driver = webdriver.Chrome(options=chrome_options)
+            email = row[0].value
+            senha = row[1].value
+
+            credentials[email] = senha
+            
+        return credentials
+
+    def openChrome(self):
+        self.driver = webdriver.Chrome(options=self.chrome_options)
+        self.actions = ActionChains(self.driver)
         self.driver.maximize_window()
-
         self.wait = WebDriverWait(self.driver, 10)
 
-    def obtainEmails(self, spreadsheet, rMax):
-        for linha in spreadsheet.iter_rows(min_row=2, max_row=rMax, min_col=2, max_col=3):
-
-            email = linha[0].value
-            senha = linha[1].value
-            
-            return email, senha
-
-
     def openBing(self, account, password):
-        self.actions.key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys("n").key_up(Keys.CONTROL).key_up(Keys.SHIFT).perform()
 
         self.driver.get("https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=151&id=264960&wreply=https%3a%2f%2fwww.bing.com%2fsecure%2fPassport.aspx%3fedge_suppress_profile_switch%3d1%26requrl%3dhttps%253a%252f%252fwww.bing.com%252f%253fcc%253dbr%2526wlexpsignin%253d1%26sig%3d1C60459059DB624322E751E558B263A0%26nopa%3d2&wp=MBI_SSL&lc=1046&CSRFToken=f32eb264-e4cc-4043-a1c6-6a14b52819e6&cobrandid=c333cba8-c15c-4458-b082-7c8ce81bee85&aadredir=1&nopa=2")
         email_field = self.wait.until(EC.visibility_of_element_located((By.ID, "i0116")))
@@ -61,7 +63,7 @@ class Chrome():
 
         self.wait.until(EC.url_contains("https://www.bing.com"))
 
-    def startSearch(self, textToSearch):
+    def startSearch(self, textToSearch, loops):
         search_field = self.wait.until(EC.visibility_of_element_located((By.ID, "sb_form_q")))
         search_field.send_keys(textToSearch)
         search_field.submit()
@@ -72,28 +74,53 @@ class Chrome():
             for _ in range(2):
                 self.driver.find_element(By.ID, "sb_form_q").send_keys(Keys.DELETE)
             self.driver.find_element(By.ID, "sb_form_q").send_keys(Keys.ENTER)
-            time.sleep(8)
+            time.sleep(2)
+
 
     def dayStreak(self):
         self.driver.get("https://rewards.bing.com/?ref=rewardspanel")
 
-        elements = self.driver.find_elements(By.CLASS_NAME, 'ds-card-sec')
-        for link in elements[:3]:
+        elements = self.driver.find_elements(By.CSS_SELECTOR, '.actionLink.x-hidden-vp1')
+        for link in elements:
             if link.is_enabled():
                 link.click()
-                time.sleep(8)
+                time.sleep(5)
                 self.actions.key_down(Keys.CONTROL).send_keys("w").key_up(Keys.CONTROL).perform()
                 time.sleep(2)
 
     def closeBing(self):
-        self.actions.key_down(Keys.ALT).send_keys(Keys.F4).key_up(Keys.ALT).perform()
+        time.sleep(2)
+        self.driver.switch_to.window(self.driver.window_handles[0])
+        time.sleep(2)
+        self.driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.ALT + Keys.F4)
+
+    def getAccounts(self):
+        spreadsheet = self.archive["Accounts"]
+        rMin = spreadsheet["F3"].value + 1
+        rMax = spreadsheet["F4"].value + 1 
+        loops = spreadsheet["F5"].value
+        return rMin, rMax, loops
+
+    def getTexts(self):
+        spreadsheet = self.archive["Texts"]
+        column = spreadsheet["H4"].value + 1
+        row = spreadsheet["H5"].value + 1
+        text = spreadsheet.cell(row=row, column=column).value
+        return text
 
     def bingLoop(self):
-        self.obtainEmails(self.spreadsheet, self.rMax)
 
-        for account, password in range(5):
+        text = self.getTexts()
+        rMin, rMax, loops = self.getAccounts()
+
+        credentials = self.obtainEmails(rMin, rMax)
+
+        for account, password in credentials.items():
+            self.openChrome()
             self.openBing(account, password)
-            self.startSearch(self.textToSearch)
+            self.startSearch(text, loops)
             self.dayStreak()
             self.closeBing()
             time.sleep(3)
+
+chrome = Chrome()
